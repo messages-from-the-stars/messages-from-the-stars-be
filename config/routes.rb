f7d20d25005c367bcc71380104bd94d460636677
Thiago Silveira <thiago1992@yahoo.com>
2022-09-15 17:38:14 -0600

    Add: Satellites find by user id route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get "/satellites/find_by_user_id", to: "satellites#find_by_user_id"
       get "/messages/find_by_sat_id", to: "messages#find_by_sat_id"
-
+      
       resources :satellites, only: [:show]
     end
   end

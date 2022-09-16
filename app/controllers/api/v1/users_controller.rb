c7669299901f334f70e73658adbb58853da87a1f
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-15 23:10:05 -0400

    Refactor: return user json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,10 +3,9 @@ class Api::V1::UsersController < ApplicationController
   def find_or_create_user
     if params[:name].present? && params[:username].present?
       if User.find_by(username: params[:username]) != nil
-        render status: 200
+        user_json_response(User.find_by(username: params[:username]))
       else
-        User.create(username: params[:username], name: params[:name])
-        render status: 200
+        user_json_response(User.create(username: params[:username], name: params[:name]))
       end
     else
       render status: 400
